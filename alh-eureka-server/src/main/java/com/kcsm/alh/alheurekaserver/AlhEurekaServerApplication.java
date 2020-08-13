44ccdad8628ad125b128eed11a9b859762c07100
chengxi <734605725@qq.com>
2020-08-13 13:00:04 +0800

    eureka 更新配置

--- alh-eureka-server/src/main/java/com/kcsm/alh/alheurekaserver/AlhEurekaServerApplication.java
+++ alh-eureka-server/src/main/java/com/kcsm/alh/alheurekaserver/AlhEurekaServerApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class AlhEurekaServerApplication {
 
     public static void main(String[] args) {
